baa1c237edd2204a3b3406cf0376775cab825697
desarrollador4@azsmart.net <desarrollador4@azsmart.net>
2025-02-28 09:23:20 -0500

    starting setting in project with randomfox exportation

---
+++ styles/components/RandomFox.tsx
@@ -0,0 +1,16 @@
+/* import type { FunctionComponent, FC } from "react" */
+
+export const RandomFox = (): JSX.Element => {
+    return <img />
+}
+/* export const RandomFox = () => {
+    return <img />
+}
+
+export const RandomFox: FunctionComponent = ():  => {
+    return <img />
+}
+
+export const RandomFox: FC = ():  => {
+    return <img />
+} */
